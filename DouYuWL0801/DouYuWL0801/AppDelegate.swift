19cafe6397e11062b73727510794833158c632d9
Vinlor <13824380425@163.com>
2017-08-01 15:34:15 +0800

    设置项目层次结构

---
+++ DouYuWL0801/DouYuWL0801/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DouYuWL0801
+//
+//  Created by dinpay on 2017/8/1.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
---
+++ DouYuWL0801/DouYuWL0801/Profile/Controller/DiscoverViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DiscoverViewController.swift
+//  DouYuWL0801
+//
+//  Created by dinpay on 2017/8/1.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
